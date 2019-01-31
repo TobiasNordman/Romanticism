e1543b82be0af27c68bc95aceb35e90b19206052
TobiasNordman <Drtboy01@gmail.com>
2019-01-31 10:33:38 +0100

    no

--- Romanticism.js
+++ Romanticism.js
@@ -1,5 +1,12 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+<script>
 
 $.get('text.txt',function(data){
   document.getElementById("pone").innerHtml = data;
 });
+
+</script>
