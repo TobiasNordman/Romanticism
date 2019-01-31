00a220708d1e15bf8461cdd283180d9ae613d8d5
TobiasNordman <Drtboy01@gmail.com>
2019-01-31 10:43:37 +0100

    javascripshiet

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Romanticism
 Wololo my child
 https://tobiasnordman.github.io/Romanticism/
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+<script>
+
+$.get('insertname.txt', function(data){
+  document.getElementById("idofyourshiet").innerHtml = data;
+});
+
+</script>
--- Romanticism.js
+++ Romanticism.js
@@ -1,12 +1,9 @@
-
-
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
 </script>
 <script>
 
-$.get('text.txt',function(data){
-  document.getElementById("pone").innerHtml = data;
+$.get('insertname.txt', function(data){
+  document.getElementById("idofyourshiet").innerHtml = data;
 });
 
 </script>
