03578f94fc0d39b4188d3130b98d5c68d8758a83
TobiasNordman <Drtboy01@gmail.com>
2019-01-31 10:54:29 +0100

    I HAVE AUTISM

--- Romanticism.js
+++
@@ -1,6 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
-</script>
-
-$.get('insertname.txt', function(data){
-  document.getElementById("idofyourshiet").innerHtml = data;
-});
