eeae70de489ea6957adfe7729e79d14bd8048346
TobiasNordman <Drtboy01@gmail.com>
2019-01-31 10:49:58 +0100

    js

--- Romanticism.js
+++ Romanticism.js
@@ -1,9 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
 </script>
-<script>
 
 $.get('insertname.txt', function(data){
   document.getElementById("idofyourshiet").innerHtml = data;
 });
-
-</script>
